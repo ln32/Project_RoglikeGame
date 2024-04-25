829e4fe6106b323e80772414689e1e0f70e741d3
ln32 <jhchoi602@gmail.com>
2024-04-25 18:54:32 +0900

    Remove to Apply

--- InvenFunc/EventFunc/_EventFunc_InvenCtrl.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-internal static class _EventFunc_InvenCtrl
-{
-}
--- InvenFunc/MapInven/Gui/Set/Level 2/0. MangeRDM/RDM_Default/RDM_First/EventSC_01.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using static GUI_MapScenario;
-
-public class EventSC_01 : EventSC_Default
-{
-
-}
